4d57d1b4d8cee1edb87daf503f040538548d1304
Daisuke Ii <dai@e-frontier.systems>
2024-09-06 21:26:28 +0900

    9.1.1のバイナリに対応 build.sh起動時にパスワードプロンプト表示 ctool.sh実装開始

--- airgap/build.sh
+++ airgap/build.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-read -n 1 -p "エアギャップのパスワードをデフォルトから変更しますか？ [Yn]" Yn
+read -n 1 -p "エアギャップのパスワードをデフォルトから変更しますか？ [Yn]: " Yn
+
+
+export_password() {
+    while read -sp "Password: " password; do
+        echo
+        if [ ${#password} -lt 8 ]; then
+            echo "パスワードの長さは8文字以上にしましょう!"
+            continue
+        fi
+        read -sp "Password(確認): " repeat
+        if [ "${password}" = ${repeat} ]; then
+            export PASSWD=$password
+            return
+        else
+            echo "パスワードが一致しません。"
+            echo
+        fi
+    done
+}
+
 
 case $Yn in
     "" | [Yy]* )
-        read -sp "Password: " password
-        export PASSWD=$password
+        export_password
     ;;
     * )
         export PASSWD=airgap
