75236e84508758daa145cda347285f5ee5c8ab40
e-frontier systems <81949305+e-frontier-systems@users.noreply.github.com>
2024-11-09 15:52:36 +0900

    1.1.9 (#71)
    * Features/build sh append result code check (#70)
    
    * Dockerが立ち上がらない不具合を修正しました
    
    * Dockerfileの不具合を修正しました
    
    * Dockerfileに余計なコピーコマンドが含まれていた不具合を修正
    
    * ctool.shの微修正
    
    * build.shに戻り値チェックを実装
    
    ---------
    
    Co-authored-by: Ii Daisuke <dai@e-frontier.systems>
    
    * Fix/ctool 0.6.10 bugfix (#69)
    
    * Dockerが立ち上がらない不具合を修正しました
    
    * Dockerfileの不具合を修正しました
    
    * Dockerfileに余計なコピーコマンドが含まれていた不具合を修正
    
    * ctool.shの微修正
    
    ---------
    
    Co-authored-by: Ii Daisuke <dai@e-frontier.systems>
    
    * Fix/dockerfile unknown copy (#68)
    
    * Dockerが立ち上がらない不具合を修正しました
    
    * Dockerfileの不具合を修正しました
    
    * Dockerfileに余計なコピーコマンドが含まれていた不具合を修正
    
    ---------
    
    Co-authored-by: Ii Daisuke <dai@e-frontier.systems>
    
    ---------
    
    Co-authored-by: DAISUKE <dai@iil.jp>

--- airgap/build.sh
+++ airgap/build.sh
@@ -4,4 +4,17 @@ echo
 echo "Dockerのビルドを開始しています..."
 echo
 echo
+
 docker compose build --no-cache
+RESULT=$?
+
+if [ "$RESULT" -ne 0 ]; then
+    echo
+    tput setaf 1 && echo "Dockerのビルドに失敗しました..." && tput setaf 7
+    echo "  ・インターネットに接続出来ているか確認してください。"
+    echo "  ・時間をおいてから再度お試しください。"
+    echo
+    exit "$RESULT"
+fi
+
+exit 0
