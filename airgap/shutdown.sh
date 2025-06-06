d5995f124ccd96415c8dcc849d9573c4ad2bb357
Ii Daisuke <dai@e-frontier.systems>
2025-06-06 18:06:40 +0900

    fix: add error messages

--- airgap/shutdown.sh
+++ airgap/shutdown.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 docker compose down
+if [ $? -ne 0 ]; then
+    echo "Failed to shut down the Docker containers."
+    exit 1
+fi
--- airgap/stop.sh
+++ airgap/stop.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 docker compose stop
+if [ $? -ne 0 ]; then
+    echo "Failed to stop the Docker containers."
+    exit 1
+fi
